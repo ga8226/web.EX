7d3f7c5cbe896af08ef3b6852ba48c068011f34f
ga8226 <ga8226@naver.com>
2023-05-12 10:29:19 +0900

    예제

---
+++ js/basic.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+
+})
+// 아이디가 detail이라는 객체의 a태그를 클릭하면 실행하라
+// $("#detail a").click(function(){})
+
+// 아이디가 slider 첫번째 li를 클릭하면 실행하라
+// $("#slider li:nth-child(1)").click(function(){})
+
+// 아이디가 slider 첫번째 li안의 자식 a 태그를 클릭하면 실행하라
+// $("#slider li:nth-child(1)>a").click(function(){})
+
+// var num =0; 초기값 num을 만들고 0을 대입해라
+// $("#slider li").eq(num).find(">a").click(function(){})
+
+// 알아둬여 하는 메서드
+// 주어용 메서드 : eq(숫자) , find(자식객체)
+// 동사용 메서드: addClass(클래스명),removeClass(클래스명),toggleClass(클래스명)
+
+// 알아둬야 하는 이벤트
+// ready : 선택자가 오직 하나 
+// click :모든 객체 해당
+// submit : 폼에 있는 input, button만 해당
+// change :폼에 있는 input만 해당
